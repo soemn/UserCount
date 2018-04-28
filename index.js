2911f58e673e4513f90c5be48d356b1b0683e4c6
sm <a0086845@u.nus.edu>
2018-04-28 23:22:58 +0800

    Allowed cors

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 const http = require("http").Server(app)
 var AWS = require("aws-sdk")
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
 AWS.config.update({
   region: "ap-northeast-1",
   credentials: new AWS.Credentials(
